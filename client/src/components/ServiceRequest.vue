d1467ef30d9a10bde3eaaf173e87790a93677ad2
Brendon Willian <bmota@its.jnj.com>
2018-06-05 17:09:31 -0300

    Added bootstrap

--- client/src/components/ServiceRequest.vue
+++ client/src/components/ServiceRequest.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
-    It is working
-  </div>
+    <b-table striped hover :items='items' :fields='fields'>
+      <template slot='street' slot-scope='row'>
+        <b-form-input autocomplete="true" v-model="row.item.street"
+          type="text" placeholder="Entre com o endereço"></b-form-input>
+      </template>
+      <template slot='number' slot-scope='row'>
+        <b-form-input v-model="row.item.number" type="number"
+          placeholder="Entre com o numero da casa"></b-form-input>
+      </template>
+      <template slot='note' slot-scope='row'>
+        <b-form-textarea v-model="row.item.note"
+          placeholder="Entre com alguma observação">
+        </b-form-textarea>
+      </template>
+    </b-table>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+  data() {
+    return {
+      items: [
+        { street: "Rua Busca Aqui", number: 1, note: "Casa B" },
+        { street: "Rua Leva Aqui", number: 2, note: null }
+      ],
+      fields: [
+        { key: "street", label: "Rua" },
+        { key: "number", label: "Número" },
+        { key: "note", label: "Observação" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.b-table {
+  width: 50%;
+}
+</style>
